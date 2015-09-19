14924c1de448e718ed9477b9fdda7df64ab599fd
Peter Kaleta <mail@peterkaleta.com>
2015-09-19 13:25:13 -0500

    Release 0.3.5

--- dist/decorators/styleDefaults.js
+++ dist/decorators/styleDefaults.js
@@ -8,25 +8,34 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 exports['default'] = styleDefaults;
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var _lodash = require('lodash');
+
+var _react = require('react');
 
 function styleDefaults() {
   var defaultStyles = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
   return function (DecoratedComponent) {
+    return (function (_Component) {
+      _inherits(StyledComponent, _Component);
 
-    return (function () {
-      function _class() {
-        _classCallCheck(this, _class);
+      function StyledComponent() {
+        _classCallCheck(this, StyledComponent);
+
+        _get(Object.getPrototypeOf(StyledComponent.prototype), 'constructor', this).apply(this, arguments);
       }
 
-      _createClass(_class, [{
+      _createClass(StyledComponent, [{
         key: 'render',
         value: function render() {
           var _props = this.props;
@@ -36,12 +45,13 @@ function styleDefaults() {
           var restProps = _objectWithoutProperties(_props, ['styles']);
 
           styles = (0, _lodash.extend)({}, defaultStyles, styles);
-          return React.createElement(DecoratedComponent, _extends({}, restProps, { styles: styles }));
+
+          return React.createElement(_react.Component, _extends({}, restProps, { styles: styles }));
         }
       }]);
 
-      return _class;
-    })();
+      return StyledComponent;
+    })(_react.Component);
   };
 }
 
